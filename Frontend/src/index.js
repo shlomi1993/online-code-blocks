e8eb0b4cd180783558bb0ddb65e371ea8f43e871
shlomi1993 <shlomiggl@gmail.com>
2022-12-28 21:27:17 +0200

    unify projects

---
+++ Frontend/src/index.js
@@ -0,0 +1,11 @@
+// Written by Shlomi Ben-Shushan.
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <App />
+);
