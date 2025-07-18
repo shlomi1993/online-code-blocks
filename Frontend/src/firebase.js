f2dc69a9db51f8a75379e7da703529f2288d8039
Shlomi Ben Shushan <shlomiggl@gmail.com>
2025-07-18 15:43:20 +0300

    WIP

--- Frontend/src/firebase.js
+++
@@ -1,9 +0,0 @@
-// Written by Shlomi Ben-Shushan.
-
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = require('./config.json').firebase;
-
-const firebase = initializeApp(firebaseConfig);
-
-export default firebase;
